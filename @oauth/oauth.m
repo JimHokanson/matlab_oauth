63b53a30c97eda03edc888e07743b71f6c05e6a2
Jim Hokanson <jim.hokanson@gmail.com>
2013-05-30 00:49:24 -0400

    Old code commit before refactor

---
+++ @oauth/oauth.m
@@ -0,0 +1,12 @@
+classdef oauth < handle_light
+    %UNTITLED Summary of this class goes here
+    %   Detailed explanation goes here
+    
+    properties
+    end
+    
+    methods
+    end
+    
+end
+
